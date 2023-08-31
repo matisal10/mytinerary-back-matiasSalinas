ff4fcbc57622f435c157fb1cb30e7072ff3ba821
MatiSal <salinasmatias100@gmail.com>
2023-08-31 16:00:45 -0300

    add routes and change shcema

--- models/Itineraries.js
+++ models/Itineraries.js
@@ -5,9 +5,10 @@ let collection = "Itinerary";
 const itinerarySchema = new Schema({
     name: { type: String, required: true },
     guideName: { type: String, required: true },
-    photo: { type: String, required: true },
+    guidePhoto: { type: String, required: true },
     price: { type: Number, min: 1, max: 5, required: true },
     duration: { type: Number, required: true },
+    cover: {type: String, required: true},
     likes: { type: Number, default: 0 },
     hashtags: [{ type: String }],
     images: [{ type: String }],
--- routes/itinerary.js
+++ routes/itinerary.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import itineraryController from '../controllers/itineraries/itineraryController.js'
-const {getAllItineraries,getAllItinerariesForCity, createItineraryForCity} = itineraryController
+const {getAllItineraries,getAllItinerariesForCity, createItineraryForCity, deleteItinerary, updateItinerary} = itineraryController
 const router = express.Router();
 
 router.get('/', getAllItineraries);
@@ -9,6 +9,9 @@ router.get('/:id', getAllItinerariesForCity);
 
 router.post('/create/:id', createItineraryForCity);
 
+router.put('/:id', updateItinerary)
+
+router.delete('/:itineraryId', deleteItinerary);
 
 
 export default router;
